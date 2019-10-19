a33e82891eababa4e7bd542010ad180ee3ccc1be
budoyong <42744046+budoyong@users.noreply.github.com>
2019-10-20 03:01:01 +0800

    Import Material Modules

--- CERecipes/src/app/app-material.module.ts
+++ CERecipes/src/app/app-material.module.ts
@@ -1,12 +1,66 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
-
+import {
+  MatInputModule,
+  MatDialogModule, MatSortModule,
+  MatTableModule, MatSelectModule,
+  MatOptionModule, MatButtonModule,
+  MatCheckboxModule, MatToolbarModule,
+  MatListModule, MatSidenavModule,
+  MatCardModule, MatTabsModule,
+  MatIconModule, MatTreeModule, MatMenuModule,
+  MatExpansionModule, MatRadioModule, MatSnackBarModule
+} from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    MatExpansionModule,
+    MatTreeModule,
+    MatTabsModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatTableModule,
+    MatSortModule,
+    MatDialogModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSnackBarModule,
+    MatMenuModule
+
+  ],
+  exports: [
+    MatExpansionModule,
+    MatTreeModule,
+    MatTabsModule,
+    MatSortModule,
+    MatTableModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatToolbarModule,
+    MatListModule,
+    MatSidenavModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    FlexLayoutModule,
+    MatIconModule,
+    MatOptionModule,
+    MatSelectModule,
+    MatDialogModule,
+    MatInputModule,
+    MatRadioModule,
+    MatSnackBarModule,
+    MatMenuModule
   ]
 })
 export class AppMaterialModule { }
